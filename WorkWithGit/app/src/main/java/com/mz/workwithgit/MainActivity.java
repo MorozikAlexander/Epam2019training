6a100513feb064379078cf33ee1ef71642507cd3
MorozikAlexander <morozik.alexander@gmail.com>
2019-02-21 17:40:37 +0300

    another commit in master

--- WorkWithGit/app/src/main/java/com/mz/workwithgit/MainActivity.java
+++ WorkWithGit/app/src/main/java/com/mz/workwithgit/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Version 2
-        //2 button Version Master
+        //2 button Version Master + another commit
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
