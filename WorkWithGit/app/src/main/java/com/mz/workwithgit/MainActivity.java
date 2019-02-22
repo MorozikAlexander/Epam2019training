5d7c0262641d30676648b1ee2aa3027b07af6a54
MorozikAlexander <morozik.alexander@gmail.com>
2019-02-22 11:09:05 +0300

    Module Version 2

--- WorkWithGit/app/src/main/java/com/mz/workwithgit/MainActivity.java
+++ WorkWithGit/app/src/main/java/com/mz/workwithgit/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Version 2
-        //2 button Version Master + another commit
+        //link to another SHA submodule
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
